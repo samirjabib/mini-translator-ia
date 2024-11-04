811e42396016f69da298ce1f37c1121fd3aa0adc
Samir <98964077+samirjabib@users.noreply.github.com>
2024-11-04 02:56:53 -0500

    finish translator

--- app/api/translate/route.ts
+++ app/api/translate/route.ts
@@ -1,17 +1,41 @@
 import { openai } from "@/app/lib/open-ai";
 
+type Prompt = {
+  text: string;
+  from: string;
+  to: string;
+};
+
 export async function POST(request: Request) {
-  const prompt: string = await request.json();
-  const res = await openai.completions.create({
-    model: "davinci-002",
-    prompt,
-    temperature: 0.7,
-    max_tokens: 100,
+  const { text, from, to }: Prompt = await request.json();
+
+  const prompt = `Translate the following text from ${from} to ${to}: ${text}`;
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a expert translator and only reply with the translated text in the language specified  in the prompt and nothing else in the response. The language is specified in the prompt. You must not add any other text to the response.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    max_completion_tokens: 500,
+    temperature: 0.3,
   });
 
-  console.log(res);
+  const response = JSON.stringify({
+    translatedText: completion.choices[0].message.content,
+  });
 
-  return new Response("Hello, Next.js!", {
+  return new Response(response, {
     status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 }
